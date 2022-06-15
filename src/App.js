c67577f1d7b70ca0ca8353b8eba719883f4f0991
Jidonu Segun <jidonu2@gmail.com>
2022-06-15 13:32:29 +0100

    netflix homepage design using react and pwa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from "@reach/router"
+import Home from './pages/home';
+import './styles/style.css'
+import TopPicks from './pages/top-picks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Home path="/"/>
+      <TopPicks path="top-picks"/>
+    </Router>
   );
 }
 
